f3206e2362642badf33241996ff5dd3fcfd89fdd
MirellyN <mirelly.naianny@dce.ufpb.br>
2020-02-13 20:08:51 -0300

    Testes 1 e 2  Passando

---
+++ src/main/java/br/ufpb/dcx/aps/escalonador/MaisCurtoFactory.java
@@ -0,0 +1,15 @@
+package br.ufpb.dcx.aps.escalonador;
+
+public class MaisCurtoFactory implements AbstractFactory{
+
+	@Override
+	public Escalonador criarEscalonador() {
+		return new MaisCurto();
+	}
+
+	@Override
+	public Escalonador criarEscalonador(int quantum) {
+		return new MaisCurto(quantum);
+	}
+
+}
