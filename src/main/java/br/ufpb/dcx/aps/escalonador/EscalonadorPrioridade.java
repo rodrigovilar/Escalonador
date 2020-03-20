5eb757ba7b67c5f79e08bc2656426c9cc8ae90f1
DAVICORREIA21 <davicorreia21@gmail.com>
2020-03-20 19:14:56 -0300

    add metodo tick

--- src/main/java/br/ufpb/dcx/aps/escalonador/EscalonadorPrioridade.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/EscalonadorPrioridade.java
@@ -1,6 +1,7 @@
 package br.ufpb.dcx.aps.escalonador;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class EscalonadorPrioridade extends EscalonadorBase {
 	public EscalonadorPrioridade() {
@@ -24,6 +25,37 @@ public class EscalonadorPrioridade extends EscalonadorBase {
 		this.tipoEscalonador = TipoEscalonador.Prioridade;
 		this.listaDeProcessosBloqueados = new ArrayList<>();
 		this.quantum = quantum;
+	}
+	public void tick() {
+
+		tick = tick + (tick - (tick - 1));// Piada Interna entre os intregrantes
+
+		if (finalizado) this.finalizandoProcesso(this.nomeFinalizado);
+			this.finalizado = false;
+
+		if (processoBloqueado)
+			this.bloqueandoProcesso(nomeProcessoBloquado);
+
+		if (retomar) this.retomandoProcesso(listaDeProcessosRetomados);
+			this.retomar = false; Collections.sort(ListaDeProcessos);
+		
+		if (rodando != null) rodando.addtempo();
+
+		if (rodando != null && rodando.getAtual() >= quantum && Proximo < tick)
+			this.trocaParaFila();
+
+		if (this.rodando == null && !ListaDeProcessos.isEmpty())
+			this.adicionarProcessoRodando();
+
+		if ( retomar == false && !listaDeProcessosBloqueados.isEmpty()) {
+			this.processoBloqueado = true; this.trocaParaFila();
+			this.adicionarProcessoRodando();
+		}
+
+		if (!ListaDeProcessos.isEmpty() && ListaDeProcessos.get(0).getPrioridade() < rodando.getPrioridade()) {
+			if (rodando != null) this.trocaParaFila(); this.adicionarProcessoRodando();
+				Collections.sort(ListaDeProcessos);		
+		}
 	}
 
 }
