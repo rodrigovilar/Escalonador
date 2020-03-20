864cda98902d246afc3693f3482cef5e38303287
Beatriz Maciel <56920708+beatrizstar@users.noreply.github.com>
2020-03-20 20:38:12 -0300

    fifobase

--- src/main/java/br/ufpb/dcx/aps/escalonador/FifoBase.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/FifoBase.java
@@ -2,4 +2,13 @@ package br.ufpb.dcx.aps.escalonador;
 
 public class FifoBase  implements CriadorTipo {
 	
+	@Override
+		public EscalonadorBase criarEscalonador() {
+			return new EscalonadorFifo();
+		}
+		@Override
+		public EscalonadorBase criarEscalonador(int quantum) {
+			return new EscalonadorFifo(quantum);
+		}
+	
 }
