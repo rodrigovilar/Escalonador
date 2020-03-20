148b15326894a333bd6346d45fb6797b33d4d68a
beatrizstar <biazita026@gmail.com>
2020-03-20 18:32:53 -0300

    classes e add listas

--- src/main/java/br/ufpb/dcx/aps/escalonador/EscalonadorBase.java
+++ src/main/java/br/ufpb/dcx/aps/escalonador/EscalonadorBase.java
@@ -1,5 +1,62 @@
 package br.ufpb.dcx.aps.escalonador;
 
-public class EscalonadorBase {
+import java.util.Collections;
+import java.util.List;
 
+
+public class EscalonadorBase {
+	protected int tick, quantum, tickProximoFila;
+	protected List<Processo> fila;
+	protected List<Processo> listaBloqueados;
+	protected List<Processo> listaRetomar;
+	protected Processo rodando;
+	protected boolean finalizado, processoBloqueado, retomar = false;
+	protected String nomeProcessoFinalizado, nomeProcessoBloquado, processoRetomar;
+	protected TipoEscalonador tipoEscalonador;
+
+	
+	public String getStatus() {
+		return null;
+		
+	}
+
+	public void adicionarProcessoRodando() {
+
+	}
+
+	public void finalizarProcesso(String nomeProcesso) {
+
+	}
+
+	public void bloquearProcesso(String nomeProcesso) {
+
+		
+	}
+
+	public void retomarProcesso(String nomeProcesso) {
+
+		
+	}
+
+	public Boolean existsProcessoByName(String nome) {
+		return null;
+		
+	}
+
+	public boolean isBloqueado(String p) {
+		return false;
+		
+	}
+
+	public boolean isRodando(String p) {
+		return false;
+		
+	}
+
+	public void adicionarProcessoTempoFixo(String nomeProcesso, int duracao) {
+
+		
+	}
 }
+
+	
